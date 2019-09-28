054d38dfa31abc23bd12cccacd3bc5a08de2768d
Subhalakshmi-R <47483278+Subhalakshmi-R@users.noreply.github.com>
2019-09-28 15:41:33 +0530

    Add files via upload

---
+++ One Venue/login.php
@@ -0,0 +1,23 @@
+<?php
+
+ob_start();
+$conn = mysqli_connect("localhost:3306","root","","one_venue");
+  if(!$conn)
+  {
+      die("Not Connected..".mysqli_connect_error());
+  }
+  else
+    "Connected Properly";
+$Email=$_POST['email'];
+$Password=md5($_POST['psw']);
+$sql="select * from customers where email='$Email' and password='$Password'";
+$result=mysqli_query($conn,$sql);
+$count=mysqli_num_rows($result);
+if ($count==1){
+  header("location:login.html");
+}
+else{
+  echo "Not existing";
+}
+ob_end_flush();
+?>
